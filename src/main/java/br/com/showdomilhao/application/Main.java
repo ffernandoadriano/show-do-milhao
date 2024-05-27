ccad246de94518378829a3370c8dbc7e514dc435
ffernandoadriano <lfernando.adriano@gmail.com>
2024-05-27 09:53:02 -0300

    Classe principal que inicia a aplicação Show do Milhão.

---
+++ src/main/java/br/com/showdomilhao/application/Main.java
@@ -0,0 +1,16 @@
+package br.com.showdomilhao.application;
+
+/**
+ * Classe principal que inicia a aplicação Show do Milhão.
+ */
+public class Main {
+
+    /**
+     * O método main que lança a aplicação JavaFX.
+     *
+     * @param args os argumentos da linha de comando
+     */
+    public static void main(String[] args) {
+        ApplicationReference.main(args);
+    }
+}
